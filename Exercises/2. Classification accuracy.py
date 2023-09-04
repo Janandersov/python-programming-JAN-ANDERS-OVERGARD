0c4ed4111b174bc55d54d364b7093380c0effc0b
janandersov <janandersov@gmail.com>
2023-09-04 17:38:29 +0200

    2. Classification accuracy task updated

--- Exercises/2. Classification accuracy.py
+++ Exercises/2. Classification accuracy.py
@@ -3,4 +3,4 @@ correct_predictions = 300
 
 model_accuracy = correct_predictions / amount_of_predictions
 
-print(f"The accuracy of this model is{model_accuracy: .3f}")
+print(f"The accuracy of this model is{model_accuracy: .3f}.")

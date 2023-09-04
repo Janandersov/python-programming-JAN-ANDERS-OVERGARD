3920aaf15b70db4adf1dd44e15a36ee093176e51
janandersov <janandersov@gmail.com>
2023-09-04 17:16:48 +0200

    Pythagorean theorem - task A added

---
+++ Exercises/1. Pythagorean theorem - task B.py
@@ -0,0 +1,13 @@
+import math
+
+hypotenuse_c = 7.0
+cathetus_a = 5.0
+
+hypotenuse_c_squared = hypotenuse_c ** 2
+cathetus_a_squared = cathetus_a ** 2
+
+cathetus_b_squared = hypotenuse_c_squared - cathetus_a_squared
+
+cathetus_b = math.sqrt(cathetus_b_squared)
+
+print(f"Cathetus b is{cathetus_b: .1f} length units!")

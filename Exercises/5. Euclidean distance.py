4491791f0a8d8d656b2a8b4783700499036e238e
janandersov <janandersov@gmail.com>
2023-09-05 00:25:44 +0200

    5. Euclidean distance task added

---
+++ Exercises/5. Euclidean distance.py
@@ -0,0 +1,9 @@
+import math
+
+x_1 = 3
+y_1 = 5
+x_2 = -2
+y_2 = 4
+
+line_length = math.sqrt((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2)
+print(f"The distance between the points is around{line_length: .1f} length units.")

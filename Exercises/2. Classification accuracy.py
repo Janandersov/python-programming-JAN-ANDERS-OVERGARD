59a6abfb49d33a6df8ae5c0e759e1c638d08093d
janandersov <janandersov@gmail.com>
2023-09-04 17:29:08 +0200

    2. Classification accuracy task added

---
+++ Exercises/2. Classification accuracy.py
@@ -0,0 +1,6 @@
+amount_of_predictions = 365
+correct_predictions = 300
+
+model_accuracy = correct_predictions / amount_of_predictions
+
+print(f"The accuracy of this model is{model_accuracy: .3f}")

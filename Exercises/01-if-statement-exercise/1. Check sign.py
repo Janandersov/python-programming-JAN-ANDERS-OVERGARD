2e375378fe8118bb76339e043cf19f2f686bcd17
janandersov <janandersov@gmail.com>
2023-09-06 14:16:40 +0200

    1. check sign task updated

--- Exercises/01-if-statement-exercise/1. Check sign.py
+++ Exercises/01-if-statement-exercise/1. Check sign.py
@@ -1,2 +1,8 @@
-input("Please input a number: ")
+user_number = float(input("Please input a number: "))
 
+if user_number > 0:
+    print("You have typed a positive number!")
+elif user_number < 0:
+    print("You have typed a negative number!")
+else:
+    print("You have typed the number 0!")

277855a44df905a85c2a66487b0704be19638b72
janandersov <janandersov@gmail.com>
2023-09-07 10:12:57 +0200

    Updated 3. Right angle to account for negative angles

--- Exercises/01-if-statement-exercise/3. Right angle.py
+++ Exercises/01-if-statement-exercise/3. Right angle.py
@@ -5,8 +5,8 @@ third_angle = float(input("Please input the THIRD angle of your triangle: "))
 
 if first_angle + second_angle + third_angle != 180:
     print("\nSorry, those angles does not make a valid triangle.")
-elif first_angle == 0 or second_angle == 0 or third_angle == 0:
-    print("\nSorry, you can not make a valid triangle if an angle is zero degrees.")
+elif first_angle <= 0 or second_angle <= 0 or third_angle <= 0:
+    print("\nSorry, you can not make a valid triangle if an angle is zero degrees or less.")
 elif first_angle == 90 or second_angle == 90 or third_angle == 90:
     print("\nYour triangle has a right angle!")
 else:

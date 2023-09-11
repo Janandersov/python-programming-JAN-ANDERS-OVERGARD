b44a86b305276597c40e04f8dffad6c1a46c72b4
janandersov <janandersov@gmail.com>
2023-09-12 00:02:36 +0200

    3. Counting words added

---
+++ Exercises/05-strings-exercise/3. Counting words.py
@@ -0,0 +1,4 @@
+words_to_count = "A picture says more than a thousand words, a mathematical formula says more than a thousand pictures."
+word_list = words_to_count.split()
+
+print(f"There are {len(word_list)} words in that sentence.")

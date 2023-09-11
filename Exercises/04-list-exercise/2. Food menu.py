b27ad8e0cdf6465a407a18791504134f6bc2331a
janandersov <janandersov@gmail.com>
2023-09-11 10:47:44 +0200

    2. Food menu added

---
+++ Exercises/04-list-exercise/2. Food menu.py
@@ -0,0 +1,5 @@
+food_list = ["vegetarisk lasagne", "spaghetti", "fisk", "grönsakssoppa", "pannkakor"]
+day_list = ["Mån", "Tis", "Ons", "Tor", "Fre"]
+
+for i in range(len(food_list)):
+    print(f"{day_list[i]}: {food_list[i]}")

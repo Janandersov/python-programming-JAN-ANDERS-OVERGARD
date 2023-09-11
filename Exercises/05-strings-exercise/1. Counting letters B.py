b88bdca881e19f034f070474fd14d3f73c10bb70
janandersov <janandersov@gmail.com>
2023-09-11 11:29:36 +0200

    1. Counting letters B added

--- Exercises/05-strings-exercise/1. Counting letters B.py
+++ Exercises/05-strings-exercise/1. Counting letters B.py
@@ -2,5 +2,4 @@ user_word = str(input("Please type a word: "))
 upper_count = sum(letter.isupper() for letter in user_word)
 lower_count = sum(letter.islower() for letter in user_word)
 
-print(f"Your word has {upper_count} uppercase letters.")
-print(f"Your word has {lower_count} lowercase letters.")
+print(f"Your word has {upper_count} uppercase letter(s) and {lower_count} lowercase letter(s).")

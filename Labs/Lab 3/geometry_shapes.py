7ab8a38e4e2ccea37e47ae9e0030b07453c3190e
janandersov <janandersov@gmail.com>
2023-10-16 11:07:25 +0200

    added init/repr/str and getter/setter for shape x and y position

--- Labs/Lab 3/geometry_shapes.py
+++ Labs/Lab 3/geometry_shapes.py
@@ -1,5 +1,33 @@
 class Shape:
-    ...
+    def __init__(self, x_position: (int | float), y_position: (int | float)):
+        self.x_position = x_position
+        self.y_position = y_position
+
+    def __repr__(self) -> str:
+        return f"x = {self.x_position}, y = {self.y_position}"
+
+    def __str__(self) -> str:
+        return f"The shapes x-position is {self.x_position} and its y-position is {self.y_position}"
+
+    @property
+    def x_position(self) -> int | float:
+        return self._x_position
+
+    @x_position.setter
+    def x_position(self, value: (int | float)):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"x position has to be either int or float, not {type(value)}")
+        self._x_position = value
+
+    @property
+    def y_position(self) -> int | float:
+        return self._y_position
+
+    @y_position.setter
+    def y_position(self, value: (int | float)):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"y position has to be either int or float, not {type(value)}")
+        self._y_position = value
 
 
 class Circle(Shape):
